87083a94ba847a7edca7bf0b3c263a12f937a22d
Manish_Syal <manishsyal77@gmail.com>
2023-01-27 19:55:05 +0530

    again trying to deploy

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server -> bot's response
 
-  const response = await fetch("https://codex-gpt-rula.onrender.com/", {
+  const response = await fetch("https://codex-gpt-fcik.onrender.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
